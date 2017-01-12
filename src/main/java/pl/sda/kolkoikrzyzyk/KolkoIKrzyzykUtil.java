b326a36e97bd56097ead24ebda430ed71b24bfe5
Mateusz <mateuszwieckowski91@gmail.com>
2017-01-12 19:03:18 +0100

    Dodano numeracje pol

---
+++ src/main/java/pl/sda/kolkoikrzyzyk/KolkoIKrzyzykUtil.java
@@ -0,0 +1,23 @@
+package pl.sda.kolkoikrzyzyk;
+
+public class KolkoIKrzyzykUtil {
+
+	public static boolean isDigit(String text) {
+
+		char[] tablica = text.toCharArray();
+
+		if (tablica.length > 1) {
+
+			return false;
+		}
+
+		char znak = tablica[0];
+
+		if (znak >= 49 && znak <= 57) {
+
+			return true;
+		}
+
+		return false;
+	}
+}
